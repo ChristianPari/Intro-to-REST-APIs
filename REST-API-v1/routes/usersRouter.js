883855942df75b73c821499631fde94a41c16e2f
Christian Pari <ChristianPari@outlook.com>
2020-05-30 15:11:27 -0400

    Created PATCH req to update a user

--- REST-API-v1/routes/usersRouter.js
+++ REST-API-v1/routes/usersRouter.js
@@ -2,6 +2,8 @@ const express = require('express'),
     fs = require('fs'),
     router = express.Router(),
     getDB = require('../middleware/getDB'),
+    validID = require('../middleware/validID'),
+    validNewUser = require('../middleware/validNewUser'),
     textFile = (process.cwd() + '/database/db.txt');
 
 router.get('/', getDB, (req, res) => { // GET to the users route displays all the collection of users
@@ -24,12 +26,12 @@ router.get('/:user_id', getDB, validID, (req, res) => { // GET a specific user f
 
 router.post('/', getDB, validNewUser, (req, res) => { // POST/Create a new user
 
-    let newDB = req.db_data.users, // orginal DB but will be new once newUser is pushed to it
+    let usersDB = req.db_data.users, // orginal DB but will be new once newUser is pushed to it
         newUser = req.body;
 
-    newDB.push(newUser);
+    usersDB.push(newUser);
 
-    const stringifiedDB = JSON.stringify(newDB);
+    const stringifiedDB = JSON.stringify(usersDB);
 
     fs.writeFileSync(textFile, stringifiedDB);
 
@@ -44,7 +46,7 @@ router.post('/', getDB, validNewUser, (req, res) => { // POST/Create a new user
 
 });
 
-router.delete('/:user_id', getDB, validID, (req, res) => {
+router.delete('/:user_id', getDB, validID, (req, res) => { // DELETE a user from the DB
 
     const userData = req.user_data,
         usersDB = req.db_data.users,
@@ -66,98 +68,38 @@ router.delete('/:user_id', getDB, validID, (req, res) => {
 
 });
 
-// PATCH
-
-//* ############### Middleware for requests ###############
-function validID(req, res, next) { // this validates if the requested user ID exists, if not responds with error
+router.patch('/:user_id', getDB, validID, (req, res) => { // PATCH/Update a user from within the DB
 
     const usersDB = req.db_data.users,
-        amountOfUsers = usersDB.length,
-        requestedID = req.params.user_id;
+        userData = req.user_data,
+        userIdx = usersDB.indexOf(userData),
+        updatedUserData = req.body;
 
-    if (isNaN(requestedID)) {
+    for (const field in userData) {
 
-        return res.status(400).json({
+        if (updatedUserData[field]) {
 
-            status: 400,
-            message: `The ID you have requested in not a number, only numbers are valid ID's. Please enter an ID from the range below`,
-            users_range: `1 to ${amountOfUsers}`,
-            requested_id: requestedID
-
-        });
-
-    }
-
-    if (requestedID > amountOfUsers || requestedID < 0) {
-
-        return res.status(404).json({
-
-            status: 404,
-            message: `The user ID you have requested: ${requestedID}, is not a valid id within the database, please ensure the requested ID is within the correct range found below`,
-            users_range: `1 to ${amountOfUsers}`,
-            requested_id: requestedID
-
-        })
-
-    }
-
-    req.user_data = usersDB[requestedID - 1];
-
-    next();
-
-};
-
-function validNewUser(req, res, next) { // validates the data being created for a user; all fields are present else error
-
-    const { owner: nO, pet: nP, breed: nB, favorites: nF } = req.body,
-        newUserObj = {
-            owner: nO,
-            pet: nP,
-            breed: nB,
-            favorites: nF
-        },
-        newUserObjKeys = Object.keys(newUserObj), // used for length and their data in below code
-        reqBodyKeys = Object.keys(req.body), // used for length and their data in below code 
-        missingFields = [];
-
-    for (const field in newUserObj) {
-
-        if (!newUserObj[field]) {
-
-            missingFields.push[field];
+            userData[field] = updatedUserData[field];
 
         }
 
     }
 
-    if (reqBodyKeys.length != newUserObjKeys.length) {
+    // change the users data in the db
+    usersDB.splice(userIdx, 1, userData);
 
-        return res.status(400).json({
+    const stringifiedDB = JSON.stringify(usersDB);
 
-            status: 400,
-            message: 'Request body length requirement not met',
-            required_fields: newUserObjKeys,
-            supplied_request: reqBodyKeys
+    fs.writeFileSync(textFile, stringifiedDB);
 
-        });
+    res.status(200).json({
 
-    } else if (missingFields.length != 0) {
+        status: 200,
+        message: 'User succesfully updated',
+        updated_user: userData
 
-        return res.status(400).json({
+    });
 
-            status: 400,
-            message: 'Missing required fields',
-            required_fields: newUserObjKeys,
-            supplied_request: reqBodyKeys
-
-        });
-
-    }
-
-    req.body = newUserObj;
-
-    next();
-
-};
+});
 
 module.exports = router; //! STOP FORGETTING TO EXPORT!!!
